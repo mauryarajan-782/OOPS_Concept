6be4d9b05ec110c131ccb27a1339ff4a46458a89
mauryarajan782-design <mauryarajan782@gmail.com>
2026-08-31 09:36:26 +0530

    update

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+public class Test {
+	
+	int no;
+	
+	public Test() {
+		System.out.println("obj no.");
+		
+	}
+
+	public static void main(String[] args) {
+		
+		Test t1 = new Test();
+		
+		
+
+	}
+
+}
