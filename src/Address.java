f76766c67667c4214f7808c2618bbcf4b9b60b10
mauryarajan782-design <mauryarajan782@gmail.com>
2026-09-11 01:10:53 +0530

    update

---
+++ src/Address.java
@@ -0,0 +1,37 @@
+class Address{
+	
+	String city = "Delhi";
+	String country = "India";
+
+	void displayAddress() {
+		System.out.println("city : " + city + "country : " + country);
+	}
+	
+class Student{
+	
+	String name = "Deepak";
+	int rollno = 101;
+	
+	private Address address;
+	
+	Student(Address address){
+		this.address = address;
+	}
+	
+	void displayinfo() {
+		System.out.println("name : " + name + "rollno : " + rollno);
+		address.displayAddress();
+	}
+	
+class MainApp {
+
+		public static void main(String[] args) {
+			
+			Address address = new Address();
+			
+			Student student = new Student(address);
+			
+			student.displayinfo();
+		}
+}
+ 
